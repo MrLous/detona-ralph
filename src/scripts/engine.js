142c7fc9f75e09cee2f782934f0babfd1c2ce444
MrLous <ricardoluizjr@live.com>
2023-11-16 02:08:38 -0300

    new file:   index.html 	new file:   src/img/contacts/git.png 	new file:   src/img/contacts/git.svg 	new file:   src/img/contacts/linkdin.png 	new file:   src/img/contacts/linkdin.svg 	new file:   src/img/contacts/whatsApp.png 	new file:   src/img/contacts/whatsApp.svg 	new file:   src/img/game/bloco.png 	new file:   src/img/game/bloco.svg 	new file:   src/img/game/detonaRalph.png 	new file:   src/img/game/detonaRalph.svg 	new file:   src/img/game/logo.png 	new file:   src/img/game/logo.svg 	new file:   src/img/icone.ico 	new file:   src/scripts/engine.js 	new file:   src/styles/index.css 	new file:   src/styles/reset.css

---
+++ src/scripts/engine.js
@@ -0,0 +1,79 @@
+const state = {
+    view:{
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".emeny"),
+        timeLeft: document.querySelector("#timeLeft"),
+        lives: document.querySelector("#lives"),
+        score: document.querySelector("#score")
+    },
+
+    values:{
+        timerId: null,
+        countDownTimerId: setInterval(contDown, 1000),
+        gameVelocity: 1000,
+        hitPosition: 0,
+        score:0,
+        lives:3,
+        curretTime: 60,
+    }
+    
+};
+
+function contDown() {
+    state.values.curretTime--
+    state.view.timeLeft.textContent = state.values.curretTime;
+    if (state.values.curretTime <= 0) {
+        alert("Fim de Jogo! \n score: "+state.values.score)
+    }
+}
+
+function randomSquare(){
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    })
+    let randomNumer = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumer];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+function moveEnemy() {
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity)
+}
+
+function resetGame(){
+    state.values.lives = 3
+    state.view.lives.textContent = "x" + state.values.lives;
+    state.values.curretTime = 60
+    state.view.timeLeft.textContent = state.values.curretTime;
+    state.values.score = 0;
+    state.view.score.textContent = state.values.score;
+
+}
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition){
+                state.values.score++;
+                state.view.score.textContent = state.values.score;
+                state.values.hitPosition=null;
+            }else{
+                state.values.lives--;
+                state.view.lives.textContent = "x" + state.values.lives;
+                state.values.hitPosition=null;
+                if (state.values.lives < 0) {
+                    alert("Game Over! \n score: "+state.values.score)
+                    resetGame();
+                }                
+            }
+        })
+    })
+}
+
+function initialize() {
+    moveEnemy();
+    addListenerHitBox();
+}
+
+initialize();
