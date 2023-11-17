2a07ed601566345b582cd8a53203a753cf95779d
Ricardo Luiz Junior <94785832+MrLous@users.noreply.github.com>
2023-11-17 00:00:15 -0300

    Update engine.js
    setting the tempo for the final song

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -87,7 +87,7 @@ function contDown() {
         state.view.squares[4].classList.remove("init");
     }
     //sound for end of game
-    if (state.values.curretTime == 15) {
+    if (state.values.curretTime == 28) {
         soudsFinalTime.play();
         state.values.gameVelocity = 400;
 
